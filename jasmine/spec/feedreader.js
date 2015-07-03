d2973b73ced8c111a1f5fb4778f2d5328d4fff39
Matthew Wells <matthewpwells@gmail.com>
2015-07-03 12:18:34 -0700

    Clean up code corrections (i.e. change for-in to forEach, change spaces to tabs, separate URL and name check)

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,32 +9,30 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
-		 //Make sure all feeds is defined and it has entries
+        //Make sure all feeds is defined and it has entries
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-		//test that each element has a name and a URL
+        //test that each element has a name
         it('has name', function() {
-            for (feed in allFeeds) {
-                expect(allFeeds[feed].name).toBeDefined();
-                expect(allFeeds[feed].name).not.toBe(null);
-                expect(allFeeds[feed].url).toBeDefined();
-                expect(allFeeds[feed].url).not.toBe(null);
-            }
+            allFeeds.forEach(function(e){
+                expect(e.name).toBeDefined();
+                expect(e.name).not.toBe(null);
+                expect(e.name).not.toBe('');
+            });
+        });
+
+        //test that each element has a URL
+        it('has URL', function() {
+            allFeeds.forEach(function(e){
+                expect(e.url).toBeDefined();
+                expect(e.url).not.toBe(null);
+                expect(e.url).not.toBe('');
+            });
         });
     });
 
@@ -42,7 +40,7 @@ $(function() {
     describe('The menu', function() {
         var bodyClass = $('body').hasClass('menu-hidden');
 
-		//make sure the menu starts as hidden
+        //make sure the menu starts as hidden
         it ('hidden menu', function() {
 
             bodyClass = $('body').hasClass('menu-hidden');
@@ -50,7 +48,7 @@ $(function() {
             expect(bodyClass).toBe(true);
         });
 
-		//make sure the menu shows on first click
+        //make sure the menu shows on first click
         it ('menu shows on first click', function() {
             $('.menu-icon-link').trigger('click');
             bodyClass = $('body').hasClass('menu-hidden');
@@ -58,7 +56,7 @@ $(function() {
             expect(bodyClass).toBe(false);
         });
 
-		//make sure the menu hides on the second click
+        //make sure the menu hides on the second click
         it ('menu hides on second click', function() {
             $('.menu-icon-link').trigger('click');
             bodyClass = $('body').hasClass('menu-hidden');
@@ -70,12 +68,12 @@ $(function() {
 
     describe('Initial Entries',function() {
 
-		//function to make sure test occurs even though function is async
+        //function to make sure test occurs even though function is async
         beforeEach(function(done) {
-                loadFeed(0, done);
-            });
+            loadFeed(0, done);
+        });
 
-		//make sure there is at least one entry in the loadFeed function
+        //make sure there is at least one entry in the loadFeed function
         it('At least one entry loads in feed', function(done) {
             expect($('.feed').children().length).not.toBe(0);
             done();
@@ -86,7 +84,7 @@ $(function() {
         var feedTitle;
         var feedName;
 
-		//function to make sure test occurs even though function is async
+        //function to make sure test occurs even though function is async
         beforeEach(function(done) {
             loadFeed(0, function() {
                 feedTitle = $('.feed .entry h2').html();
@@ -97,14 +95,14 @@ $(function() {
             });
         });
 
-		//make sure the content actually changes
+        //make sure the content actually changes
         it('loadFeed changes content', function(done) {
             expect($('.feed .entry h2').html()).not.toBe(feedTitle);
             done();
         });
 
-		//make sure the header actually changes
-		it('loadFeed changes header', function(done) {
+        //make sure the header actually changes
+        it('loadFeed changes header', function(done) {
             expect($('h1.header-title').html()).not.toBe(feedName);
             done();
         });
